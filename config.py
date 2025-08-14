282249c0d4e4eb4d3e40b60a38f63d5bb6ac2b17
кιяαи ѕєя <105064751+Kiranxer@users.noreply.github.com>
2025-08-14 07:30:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "7661317353"))
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://kevinvg006:N800YtS25a4rRSW5@cluster0.skyk86r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002487845241"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002840527179"))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002384933640"))
 
 FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "300")) # auto delete in seconds
