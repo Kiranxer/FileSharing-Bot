c1a396fb803ed480589e8dc3b96baf5909654d2f
NeonAn <justasneon@gmail.com>
2025-09-22 12:44:14 +0530

    Update user_id.py

--- plugins/user_id.py
+++ plugins/user_id.py
@@ -2,9 +2,6 @@ from pyrogram import filters, enums
 from pyrogram.types import Message
 from bot import Bot
 
-
-
-
 @Bot.on_message(filters.command("id") & filters.private)
 async def showid(client, message):
     chat_type = message.chat.type
@@ -14,8 +11,7 @@ async def showid(client, message):
         await message.reply_text(
             f"<b>__Yᴏᴜʀ Usᴇʀ ID Is__ :</b> <code>{user_id}</code>", 
             quote=True
-        )
-        
+        )        
 
 
 # MyselfNeon
