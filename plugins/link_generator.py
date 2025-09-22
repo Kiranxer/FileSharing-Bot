26f56af05182ab7e1dc168ca110d49401ea2c5aa
NeonAn <justasneon@gmail.com>
2025-09-22 12:40:32 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -1,3 +1,4 @@
+#Link_Generate.py
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
@@ -5,96 +6,64 @@ from config import ADMINS
 from helper_func import encode, get_message_id
 
 
-# ------------------ BATCH ------------------
+
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
-    uid = message.from_user.id
-
-    # ask for first message
     while True:
         try:
-            first_message = await client.ask(
-                chat_id=uid,
-                text="<b><i>Fᴏʀᴡᴀʀᴅ Tʜᴇ Fɪʀsᴛ Mᴇssᴀɢᴇ Fʀᴏᴍ DB Cʜᴀɴɴᴇʟ..\n\nOʀ Sᴇɴᴅ Tʜᴇ Lɪɴᴋ\n\nType /cancel to stop.</i></b>",
-                filters=filters.create(lambda _, __, m: m.forward_from_chat or m.text),
-                timeout=60
-            )
+            first_message = await client.ask(text = "<b><i>Fᴏʀᴡᴀʀᴅ Tʜᴇ Fɪʀsᴛ Mᴇssᴀɢᴇ Fʀᴏᴍ DB Cʜᴀɴɴᴇʟ (Wɪᴛʜ Qᴜᴏᴛᴇs)..\n\nOʀ Sᴇɴᴅ Tʜᴇ DB Cʜᴀɴɴᴇʟ Pᴏsᴛ Lɪɴᴋ</i></b>", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
-
-        if first_message.text and first_message.text.lower().startswith("/cancel"):
-            return await message.reply("<b><i>❌ Batch cancelled.</i></b>")
-
         f_msg_id = await get_message_id(client, first_message)
         if f_msg_id:
             break
         else:
-            await first_message.reply("<b><i>❌ Invalid post, try again.</i></b>", quote=True)
+            await first_message.reply("<b><i>❌ Eʀʀᴏʀ\n\nTʜɪs Fᴏʀᴡᴀʀᴅᴇᴅ Pᴏsᴛ ɪs Nᴏᴛ Fʀᴏᴍ ᴍʏ DB Cʜᴀɴɴᴇʟ ᴏʀ Tʜɪs Lɪɴᴋ ɪs Nᴏᴛ Tᴀᴋᴇɴ Fʀᴏᴍ DB Cʜᴀɴɴᴇʟ</i></b>", quote = True)
             continue
 
-    # ask for second message
     while True:
         try:
-            second_message = await client.ask(
-                chat_id=uid,
-                text="<b><i>Fᴏʀᴡᴀʀᴅ Tʜᴇ Lᴀsᴛ Mᴇssᴀɢᴇ..\n\nOʀ Sᴇɴᴅ Tʜᴇ Lɪɴᴋ\n\nType /cancel to stop.</i></b>",
-                filters=filters.create(lambda _, __, m: m.forward_from_chat or m.text),
-                timeout=60
-            )
+            second_message = await client.ask(text = "<b><i>Fᴏʀᴡᴀʀᴅ Tʜᴇ Lᴀsᴛ Mᴇssᴀɢᴇ Fʀᴏᴍ DB Cʜᴀɴɴᴇʟ (Wɪᴛʜ Qᴜᴏᴛᴇs)..\n\nOʀ Sᴇɴᴅ Tʜᴇ DB Cʜᴀɴɴᴇʟ Pᴏsᴛ Lɪɴᴋ</i></b>", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
-
-        if second_message.text and second_message.text.lower().startswith("/cancel"):
-            return await message.reply("<b><i>❌ Batch cancelled.</i></b>")
-
         s_msg_id = await get_message_id(client, second_message)
         if s_msg_id:
             break
         else:
-            await second_message.reply("<b><i>❌ Invalid post, try again.</i></b>", quote=True)
+            await second_message.reply("<b><i>❌ Eʀʀᴏʀ\n\nTʜɪs Fᴏʀᴡᴀʀᴅᴇᴅ Pᴏsᴛ ɪs Nᴏᴛ Fʀᴏᴍ ᴍʏ DB Cʜᴀɴɴᴇʟ ᴏʀ Tʜɪs Lɪɴᴋ ɪs Nᴏᴛ Tᴀᴋᴇɴ Fʀᴏᴍ DB Cʜᴀɴɴᴇʟ</i></b>", quote = True)
             continue
 
-    # generate link
+
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup(
-        [[InlineKeyboardButton("🖇️ Share URL", url=f'https://telegram.me/share/url?url={link}')]]
-    )
-    await second_message.reply_text(f"<b><i>Here is your link:</i></b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🖇️ Sʜᴀʀᴇ URL", url=f'https://telegram.me/share/url?url={link}')]])
+    await second_message.reply_text(f"<b><i>Hᴇʀᴇ ɪs Yᴏᴜʀ Lɪɴᴋ</i></b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
-# ------------------ GENLINK ------------------
+
+
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
-async def genlink(client: Client, message: Message):
-    uid = message.from_user.id
-
+async def link_generator(client: Client, message: Message):
     while True:
         try:
-            target_message = await client.ask(
-                chat_id=uid,
-                text="<b><i>Fᴏʀᴡᴀʀᴅ Mᴇssᴀɢᴇ Fʀᴏᴍ DB Cʜᴀɴɴᴇʟ..\n\nOʀ Sᴇɴᴅ Tʜᴇ Lɪɴᴋ\n\nType /cancel to stop.</i></b>",
-                filters=filters.create(lambda _, __, m: m.forward_from_chat or m.text),
-                timeout=60
-            )
+            channel_message = await client.ask(text = "<b><i>Fᴏʀᴡᴀʀᴅ Mᴇssᴀɢᴇ Fʀᴏᴍ Tʜᴇ DB Cʜᴀɴɴᴇʟ (Wɪᴛʜ Qᴜᴏᴛᴇs)..\n\nOʀ Sᴇɴᴅ Tʜᴇ DB Cʜᴀɴɴᴇʟ Pᴏsᴛ Lɪɴᴋ</i></b>", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
-
-        if target_message.text and target_message.text.lower().startswith("/cancel"):
-            return await message.reply("<b><i>❌ Cancelled.</i></b>")
-
-        msg_id = await get_message_id(client, target_message)
+        msg_id = await get_message_id(client, channel_message)
         if msg_id:
             break
         else:
-            await target_message.reply("<b><i>❌ Invalid post, try again.</i></b>", quote=True)
+            await channel_message.reply("<b><i>❌ Eʀʀᴏʀ\n\nTʜɪs Fᴏʀᴡᴀʀᴅᴇᴅ Pᴏsᴛ ɪs Nᴏᴛ Fʀᴏᴍ ᴍʏ DB Cʜᴀɴɴᴇʟ ᴏʀ Tʜɪs Lɪɴᴋ ɪs Nᴏᴛ Tᴀᴋᴇɴ Fʀᴏᴍ DB Cʜᴀɴɴᴇʟ</i></b>", quote = True)
             continue
 
-    # generate link
-    string = f"get-{msg_id * abs(client.db_channel.id)}"
-    base64_string = await encode(string)
+    base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup(
-        [[InlineKeyboardButton("🖇️ Share URL", url=f'https://telegram.me/share/url?url={link}')]]
-    )
-    await target_message.reply_text(f"<b><i>Here is your link:</i></b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🖇️ Sʜᴀʀᴇ URL", url=f'https://telegram.me/share/url?url={link}')]])
+    await channel_message.reply_text(f"<b><i>Hᴇʀᴇ ɪs Yᴏᴜʀ Lɪɴᴋ</i></b>\n\n{link}", quote=True, reply_markup=reply_markup)
+
+
+
+# MyselfNeon
+# Don't Remove Credit 🥺
+# Telegram Channel @NeonFiles
