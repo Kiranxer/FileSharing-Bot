ad5720e54b18d37b8aceb194b64d22208829d817
NeonAn <justasneon@gmail.com>
2025-09-22 12:59:24 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -1,87 +1,64 @@
-# Link_Generate.py
+#Link_Generator.py
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
 from config import ADMINS
 from helper_func import encode, get_message_id
 
-# Track active tasks per user
-active_tasks = {}  # user_id: "batch" or "genlink"
 
-# ------------------ GENLINK ------------------
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command("genlink"))
-async def genlink(client: Client, message: Message):
-    uid = message.from_user.id
-    active_tasks[uid] = "genlink"
-
-    await message.reply("<b>Forward a DB channel message or send its link.</b>")
-
-    while True:
-        m: Message = await client.listen(uid)
-
-        # Only accept forwarded messages or proper DB channel links
-        if not m.forward_from_chat and not (m.text and m.text.startswith("https://t.me/")):
-            await m.reply("<b>❌ Only forward a DB channel message or send a proper link.</b>")
-            continue
-
-        msg_id = await get_message_id(client, m)
-        if not msg_id:
-            await m.reply("<b>❌ This is not a valid DB channel post.</b>")
-            continue
-
-        # Generate link
-        base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
-        link = f"https://t.me/{client.username}?start={base64_string}"
-        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🖇️ Share URL", url=f"https://telegram.me/share/url?url={link}")]])
-        await m.reply(f"<b>Here is your link:</b>\n\n{link}", reply_markup=reply_markup)
-
-        active_tasks.pop(uid, None)
-        break
-
-# ------------------ BATCH ------------------
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command("batch"))
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
-    uid = message.from_user.id
-    active_tasks[uid] = "batch"
-
-    # First message
-    await message.reply("<b>Forward the FIRST DB channel message or send its link.</b>")
     while True:
-        first_msg: Message = await client.listen(uid)
-
-        if not first_msg.forward_from_chat and not (first_msg.text and first_msg.text.startswith("https://t.me/")):
-            await first_msg.reply("<b>❌ Only forward a DB channel message or send a proper link.</b>")
-            continue
-
-        f_msg_id = await get_message_id(client, first_msg)
+        try:
+            first_message = await client.ask(text = "<b><i>Fᴏʀᴡᴀʀᴅ Tʜᴇ Fɪʀsᴛ Mᴇssᴀɢᴇ Fʀᴏᴍ DB Cʜᴀɴɴᴇʟ (Wɪᴛʜ Qᴜᴏᴛᴇs)..\n\nOʀ Sᴇɴᴅ Tʜᴇ DB Cʜᴀɴɴᴇʟ Pᴏsᴛ Lɪɴᴋ</i></b>", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+        except:
+            return
+        f_msg_id = await get_message_id(client, first_message)
         if f_msg_id:
             break
         else:
-            await first_msg.reply("<b>❌ This is not a valid DB channel post.</b>")
-
-    # Second message
-    await first_msg.reply("<b>Forward the LAST DB channel message or send its link.</b>")
-    while True:
-        second_msg: Message = await client.listen(uid)
-
-        if not second_msg.forward_from_chat and not (second_msg.text and second_msg.text.startswith("https://t.me/")):
-            await second_msg.reply("<b>❌ Only forward a DB channel message or send a proper link.</b>")
+            await first_message.reply("<b><i>❌ Eʀʀᴏʀ\n\nTʜɪs Fᴏʀᴡᴀʀᴅᴇᴅ Pᴏsᴛ ɪs Nᴏᴛ Fʀᴏᴍ ᴍʏ DB Cʜᴀɴɴᴇʟ ᴏʀ Tʜɪs Lɪɴᴋ ɪs Nᴏᴛ Tᴀᴋᴇɴ Fʀᴏᴍ DB Cʜᴀɴɴᴇʟ</i></b>", quote = True)
             continue
 
-        s_msg_id = await get_message_id(client, second_msg)
+    while True:
+        try:
+            second_message = await client.ask(text = "<b><i>Fᴏʀᴡᴀʀᴅ Tʜᴇ Lᴀsᴛ Mᴇssᴀɢᴇ Fʀᴏᴍ DB Cʜᴀɴɴᴇʟ (Wɪᴛʜ Qᴜᴏᴛᴇs)..\n\nOʀ Sᴇɴᴅ Tʜᴇ DB Cʜᴀɴɴᴇʟ Pᴏsᴛ Lɪɴᴋ</i></b>", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+        except:
+            return
+        s_msg_id = await get_message_id(client, second_message)
         if s_msg_id:
             break
         else:
-            await second_msg.reply("<b>❌ This is not a valid DB channel post.</b>")
+            await second_message.reply("<b><i>❌ Eʀʀᴏʀ\n\nTʜɪs Fᴏʀᴡᴀʀᴅᴇᴅ Pᴏsᴛ ɪs Nᴏᴛ Fʀᴏᴍ ᴍʏ DB Cʜᴀɴɴᴇʟ ᴏʀ Tʜɪs Lɪɴᴋ ɪs Nᴏᴛ Tᴀᴋᴇɴ Fʀᴏᴍ DB Cʜᴀɴɴᴇʟ</i></b>", quote = True)
+            continue
 
-    # Generate batch link
+
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🖇️ Share URL", url=f"https://telegram.me/share/url?url={link}")]])
-    await second_msg.reply_text(f"<b>Here is your link:</b>\n\n{link}", reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🖇️ Sʜᴀʀᴇ URL", url=f'https://telegram.me/share/url?url={link}')]])
+    await second_message.reply_text(f"<b><i>Hᴇʀᴇ ɪs Yᴏᴜʀ Lɪɴᴋ</i></b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
-    active_tasks.pop(uid, None)
+
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
+async def link_generator(client: Client, message: Message):
+    while True:
+        try:
+            channel_message = await client.ask(text = "<b><i>Fᴏʀᴡᴀʀᴅ Mᴇssᴀɢᴇ Fʀᴏᴍ Tʜᴇ DB Cʜᴀɴɴᴇʟ (Wɪᴛʜ Qᴜᴏᴛᴇs)..\n\nOʀ Sᴇɴᴅ Tʜᴇ DB Cʜᴀɴɴᴇʟ Pᴏsᴛ Lɪɴᴋ</i></b>", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+        except:
+            return
+        msg_id = await get_message_id(client, channel_message)
+        if msg_id:
+            break
+        else:
+            await channel_message.reply("<b><i>❌ Eʀʀᴏʀ\n\nTʜɪs Fᴏʀᴡᴀʀᴅᴇᴅ Pᴏsᴛ ɪs Nᴏᴛ Fʀᴏᴍ ᴍʏ DB Cʜᴀɴɴᴇʟ ᴏʀ Tʜɪs Lɪɴᴋ ɪs Nᴏᴛ Tᴀᴋᴇɴ Fʀᴏᴍ DB Cʜᴀɴɴᴇʟ</i></b>", quote = True)
+            continue
+
+    base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
+    link = f"https://t.me/{client.username}?start={base64_string}"
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🖇️ Sʜᴀʀᴇ URL", url=f'https://telegram.me/share/url?url={link}')]])
+    await channel_message.reply_text(f"<b><i>Hᴇʀᴇ ɪs Yᴏᴜʀ Lɪɴᴋ</i></b>\n\n{link}", quote=True, reply_markup=reply_markup)
+
 
 
 # MyselfNeon
