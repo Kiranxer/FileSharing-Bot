78a6fde12ace473405185bda00b67265e52cfaf3
NeonAn <justasneon@gmail.com>
2025-10-08 21:35:32 +0530

    Add files via upload

---
+++ MyselfNeon.py
@@ -0,0 +1,6 @@
+class script(object):
+    # Plain text with names and URLs
+    NEON_TXT = [
+        "😕 NeonAnurag - https://t.me/MyselfNeon",
+        "😕 NeonAnurag2 - https://t.me/NeonFilesBot"
+    ]
