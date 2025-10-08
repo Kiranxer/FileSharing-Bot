ac83162c7eef21024f569e7f3135d42a9905bfa0
NeonAn <justasneon@gmail.com>
2025-10-08 21:05:44 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,6 @@
+class script(object):
+    # List of text + links
+    ANIME_TXT = [
+        "😕 <a href='https://t.me/MyselfNeon'>NᴇᴏɴAɴᴜʀᴀɢ</a>",
+        "😕 <a href='https://t.me/NeonFilesBot'>NᴇᴏɴAɴᴜʀᴀɢ2</a>"
+    ]
