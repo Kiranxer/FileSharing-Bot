e4a9e1748ae493635597b278db8d2e764c470228
NeonAn <justasneon@gmail.com>
2025-10-08 22:27:02 +0530

    Delete Neon.py

--- Neon.py
+++
@@ -1,7 +0,0 @@
-class script(object):
-    # Plain text with names and URLs
-    NEON_TXT = [
-        "😕 NeonAnurag - https://t.me/MyselfNeon",
-        "😕 NeonAnurag2 - https://t.me/NeonFilesBot"
-    ]
-  
