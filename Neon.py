de642c3df470ff0a98549b4791e389852830f504
NeonAn <justasneon@gmail.com>
2025-10-08 22:21:09 +0530

    Create Neon.py

---
+++ Neon.py
@@ -0,0 +1,7 @@
+class script(object):
+    # Plain text with names and URLs
+    NEON_TXT = [
+        "😕 NeonAnurag - https://t.me/MyselfNeon",
+        "😕 NeonAnurag2 - https://t.me/NeonFilesBot"
+    ]
+  
