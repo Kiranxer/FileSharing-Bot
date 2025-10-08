21bafbca8ffffeebbcebf310b87df27bff69bd6f
NeonAn <justasneon@gmail.com>
2025-10-08 20:32:59 +0530

    Create Script.py

---
+++ plugins/myselfneon/Script.py
@@ -0,0 +1,7 @@
+class script(object):
+    # List of text + links
+    ANIME_TXT = [
+        "😕 <a href='https://t.me/ZeroFilterBot'>NeonFilterBot</a>",
+        "😕 <a href='https://t.me/NeonFilesBot'>NeonFilesBot</a>"
+    ]
+  
