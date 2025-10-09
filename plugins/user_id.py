fa56f87df277ac5aad5c7cf06ce70c31be91ac6e
NeonAn <justasneon@gmail.com>
2025-10-09 23:36:37 +0530

    Update user_id.py

--- plugins/user_id.py
+++ plugins/user_id.py
@@ -1,11 +1,14 @@
+# UserID.py
 from pyrogram import filters, enums
 from pyrogram.types import Message
 from bot import Bot
+from config import ADMINS  # Make sure your Telegram user ID(s) are in ADMINS list
 
-@Bot.on_message(filters.command("id") & filters.private)
+@Bot.on_message(filters.command("id") & filters.private & filters.user(ADMINS))
 async def showid(client, message: Message):
     """
     Sends the user's Telegram ID in private chat.
+    Only admins can use this command.
     """
     user_id = message.from_user.id
     await message.reply_text(
