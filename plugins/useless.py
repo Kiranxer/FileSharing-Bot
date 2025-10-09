a50f77501b3546762fdada4fdbb6231ba2ea0d3f
NeonAn <justasneon@gmail.com>
2025-10-09 15:32:51 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -5,12 +5,12 @@ from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
 from helper_func import get_readable_time
 
-@Bot.on_message(filters.command('stats') & filters.user(ADMINS))
+@Bot.on_message(filters.command('uptime') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
     now = datetime.now()
     delta = now - bot.uptime
     time = get_readable_time(delta.seconds)
-    await message.reply(BOT_STATS_TEXT.format(uptime=time))
+    await message.reply(BOT_UPTIME_TEXT.format(uptime=time))
 
 @Bot.on_message(filters.private & filters.incoming)
 async def useless(_, message: Message):
