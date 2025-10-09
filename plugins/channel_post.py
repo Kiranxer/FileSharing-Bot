d81dc63b4ff70c02a3f4e61493fcaa3b8c59717a
NeonAn <justasneon@gmail.com>
2025-10-09 23:33:14 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -3,12 +3,15 @@ from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 from bot import Bot
-from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
+from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON, USER_REPLY_TEXT
 from helper_func import encode
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','uptime','anime','id']))
-async def channel_post(client: Client, message: Message):
-    # Only process if the message contains a file; ignore plain texts
+# ------------------ Admin Commands ------------------
+@Bot.on_message(
+    filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','genlink','batch','genlink','anime','id','uptime'])
+)
+async def admin_post(client: Client, message: Message):
+    # Only process if message contains media
     if not (message.document or message.video or message.audio or message.voice or message.video_note or message.photo or message.animation or message.sticker):
         return
 
@@ -28,14 +31,49 @@ async def channel_post(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🖇️ Sʜᴀʀᴇ URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("🖇️ Sʜᴀʀᴇ URL", url=f'https://telegram.me/share/url?url={link}')]]
+    )
 
-    await reply_text.edit(f"<b><i>Hᴇʀᴇ Is Yᴏᴜʀ Lɪɴᴋ 🔗</i></b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview=True)
+    await reply_text.edit(
+        f"<b><i>Hᴇʀᴇ Is Yᴏᴜʀ Lɪɴᴋ 🔗</i></b>\n\n{link}",
+        reply_markup=reply_markup,
+        disable_web_page_preview=True
+    )
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
 
 
+# ------------------ User Commands (id/uptime) ------------------
+@Bot.on_message(filters.private & ~filters.user(ADMINS) & filters.command(['id','uptime','anime','start']))
+async def user_special_commands(client: Client, message: Message):
+    cmd = message.text.split()[0][1:].lower()  # Get command without '/'
+    
+    if cmd == 'id':
+        user_id = message.from_user.id
+        await message.reply_text(
+            f"<b>__Yᴏᴜʀ Usᴇʀ ID Is__ :</b> <code>{user_id}</code>",
+            quote=True
+        )
+    elif cmd == 'uptime':
+        await message.reply_text(
+            "<b>Bot has been running smoothly! 🕒</b>",
+            quote=True
+        )
+
+
+# ------------------ User Default Reply ------------------
+@Bot.on_message(filters.private & ~filters.user(ADMINS))
+async def user_default_reply(client: Client, message: Message):
+    """
+    Any private message from a non-admin that is NOT an allowed command triggers this reply.
+    """
+    if message.text and not message.text.startswith(('/', 'id', 'uptime')):
+        await message.reply_text(USER_REPLY_TEXT, quote=True)
+
+
+# ------------------ Channel Post Handler ------------------
 @Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
 async def new_post(client: Client, message: Message):
 
@@ -46,7 +84,9 @@ async def new_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🖇️ Sʜᴀʀᴇ URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("🖇️ Sʜᴀʀᴇ URL", url=f'https://telegram.me/share/url?url={link}')]]
+    )
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
