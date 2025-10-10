895bae521dc56d8a97cc815aaf50012d2f6c83ec
NeonAn <justasneon@gmail.com>
2025-10-10 11:52:05 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,14 +1,27 @@
+#Route.py
 from aiohttp import web
-
 routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
     return web.Response(
         text="""
-        <body style="background-color:black; color:#39FF14; display:flex; justify-content:center; align-items:flex-start; height:100vh; margin:0; font-family:sans-serif; padding-top:20vh; font-size:4rem;">
-            Coded By @MyselfNeon
-        </body>
+        <body 
+    style="
+        background-color: black; 
+        color: #39FF14; 
+        display: flex; 
+        justify-content: center; 
+        align-items: flex-start; 
+        height: 100vh; 
+        margin: 0; 
+        font-family: sans-serif; 
+        padding-top: 20vh; 
+        font-size: 4rem;
+    "
+>
+    Coded By @MyselfNeon
+</body>
         """,
         content_type="text/html"
     )
@@ -17,7 +30,6 @@ async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
-
 
 # MyselfNeon
 # Don't Remove Credit 🥺
