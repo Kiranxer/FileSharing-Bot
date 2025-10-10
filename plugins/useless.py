fd8de4c10f9890429d50d78c504a31256bd736d4
NeonAn <justasneon@gmail.com>
2025-10-10 07:18:35 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,22 +1,66 @@
-from bot import Bot
-from pyrogram.types import Message
 from pyrogram import filters
-from config import ADMINS, BOT_UPTIME_TEXT, USER_REPLY_TEXT
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from datetime import datetime
+from bot import Bot
+from config import ADMINS, BOT_UPTIME_TEXT, USER_REPLY_TEXT
 from helper_func import get_readable_time
 
-@Bot.on_message(filters.command('uptime') & filters.user(ADMINS))
-async def stats(bot: Bot, message: Message):
+# ---------------------- UPTIME COMMAND ----------------------
+@Bot.on_message(filters.command("uptime") & filters.user(ADMINS))
+async def show_uptime(bot: Bot, message: Message):
+    """Show bot's uptime in a sleek format"""
     now = datetime.now()
     delta = now - bot.uptime
-    time = get_readable_time(delta.seconds)
-    await message.reply(BOT_UPTIME_TEXT.format(uptime=time))
+    uptime_str = get_readable_time(delta.seconds)
 
-@Bot.on_message(filters.private & filters.incoming)
-async def useless(_, message: Message):
+    text = f"""
+<b>🚀 ᴜᴘᴛɪᴍᴇ sᴛᴀᴛᴜs</b>
+
+<b>🕒 Rᴜɴɴɪɴɢ Sɪɴᴄᴇ:</b> <code>{bot.uptime.strftime('%Y-%m-%d %H:%M:%S')}</code>
+<b>⚡ Tᴏᴛᴀʟ Uᴘᴛɪᴍᴇ:</b> <code>{uptime_str}</code>
+
+<i>{BOT_UPTIME_TEXT if BOT_UPTIME_TEXT else "Sʏsᴛᴇᴍ sᴛᴀʙʟᴇ ᴀɴᴅ ʀᴜɴɴɪɴɢ sᴍᴏᴏᴛʜʟʏ 💫"}</i>
+"""
+
+    keyboard = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton("🔁 Refresh", callback_data="refresh_uptime"),
+            InlineKeyboardButton("📊 Stats", url="https://t.me/")
+        ]]
+    )
+
+    await message.reply_text(text, reply_markup=keyboard)
+
+
+# ---------------------- REFRESH CALLBACK ----------------------
+@Bot.on_callback_query(filters.regex("refresh_uptime"))
+async def refresh_uptime_callback(bot: Bot, query):
+    now = datetime.now()
+    delta = now - bot.uptime
+    uptime_str = get_readable_time(delta.seconds)
+
+    refreshed_text = f"""
+<b>🚀 Uᴘᴛɪᴍᴇ Rᴇғʀᴇsʜᴇᴅ!</b>
+
+<b>⚡ Cᴜʀʀᴇɴᴛ Uᴘᴛɪᴍᴇ:</b> <code>{uptime_str}</code>
+<i>🔄 Lᴀsᴛ Rᴇғʀᴇsʜ: {now.strftime('%H:%M:%S')}</i>
+"""
+    await query.message.edit_text(refreshed_text, reply_markup=query.message.reply_markup)
+
+
+# ---------------------- AUTO REPLY FOR USERS ----------------------
+@Bot.on_message(filters.private & filters.incoming & ~filters.command("uptime"))
+async def auto_reply(_, message: Message):
+    """Send friendly auto reply to private users"""
     if USER_REPLY_TEXT:
-        await message.reply(USER_REPLY_TEXT)
+        reply_text = f"""
+👋 <b>Hᴇʏ {message.from_user.first_name}!</b>
 
+{USER_REPLY_TEXT}
+
+<i>🤖 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʏᴏᴜʀ ᴛʀᴜsᴛᴇᴅ ʙᴏᴛ 💜</i>
+"""
+        await message.reply_text(reply_text)
 
 # MyselfNeon
 # Don't Remove Credit 🥺
