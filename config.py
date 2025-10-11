1297e4aaef9fe925b73f75c3826f0f116a713c7a
NeonAn <justasneon@gmail.com>
2025-10-11 06:50:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,8 @@ START_MSG = os.environ.get(
 
 FORCE_MSG = os.environ.get(
     "FORCE_SUB_MESSAGE",
-    "<b><i>Hᴇʟʟᴏ {mention} 😎\n\nTᴏ Usᴇ Tʜɪs ʙᴏᴛ ᴀɴᴅ Aᴄᴄᴇss Fɪʟᴇs ᴏʀ Fᴇᴀᴛᴜʀᴇs, ʏᴏᴜ Nᴇᴇᴅ ᴛᴏ Bᴇ ᴀ Pᴀʀᴛ ᴏғ Oᴜʀ Mᴀɪɴ Cʜᴀɴɴᴇʟ 🗓️</i></b>"
+    "<b><i>🚫 Aᴄᴄᴇss Rᴇsᴛʀɪᴄᴛᴇᴅ 🚫\nHᴇʟʟᴏ {mention} 😄\n\n"
+    "Tᴏ Usᴇ Tʜɪs ʙᴏᴛ ᴀɴᴅ Aᴄᴄᴇss Fɪʟᴇs ᴏʀ Fᴇᴀᴛᴜʀᴇs, ʏᴏᴜ Nᴇᴇᴅ ᴛᴏ Bᴇ ᴀ Pᴀʀᴛ ᴏғ Oᴜʀ Mᴀɪɴ Cʜᴀɴɴᴇʟ 🗓️</i></b>"
 )
 
 # --- Logging (Console only, no .txt files) --- #
