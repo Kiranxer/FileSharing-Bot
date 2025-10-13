1d31ea5ede2e3dc21dc039ad2d42395117cbd46c
NeonAn <justasneon@gmail.com>
2025-10-13 18:59:58 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,3 @@
-# start.py
 import os, asyncio, humanize
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
@@ -10,14 +9,12 @@ from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
-# Import NeonFiles.py
 from neonfiles import script
 
 madflixofficials = FILE_AUTO_DELETE
 jishudeveloper = madflixofficials
 file_auto_delete = humanize.naturaldelta(jishudeveloper)
 
-# ------------------ /start command for subscribed users ------------------
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
@@ -34,7 +31,80 @@ async def start_command(client: Client, message: Message):
             return
         string = await decode(base64_string)
         argument = string.split("-")
-        if len(argument) == 3:
+
+        if string.startswith("rget-"):
+            if len(argument) == 3:
+                try:
+                    start = int(int(argument[1]) / abs(client.db_channel.id))
+                    end = int(int(argument[2]) / abs(client.db_channel.id))
+                except:
+                    return
+                if start <= end:
+                    ids = range(start, end + 1)
+                else:
+                    ids = []
+                    i = start
+                    while True:
+                        ids.append(i)
+                        i -= 1
+                        if i < end:
+                            break
+            elif len(argument) == 2:
+                try:
+                    ids = [int(int(argument[1]) / abs(client.db_channel.id))]
+                except:
+                    return
+
+            temp_msg = await message.reply("<b><i>Pʟᴇᴀsᴇ Wᴀɪᴛ...⚡</i></b>")
+            try:
+                messages = await get_messages(client, ids)
+            except:
+                await message.reply_text("<b><i>Sᴏᴍᴇᴛʜɪɴɢ Wᴇɴᴛ Wʀᴏɴɢ...❌</i></b>")
+                return
+            await temp_msg.delete()
+
+            madflix_msgs = []
+
+            for msg in messages:
+                if bool(CUSTOM_CAPTION) & bool(msg.document):
+                    caption = CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html,
+                                                    filename=msg.document.file_name)
+                else:
+                    caption = "" if not msg.caption else msg.caption.html
+
+                if DISABLE_CHANNEL_BUTTON:
+                    reply_markup = msg.reply_markup
+                else:
+                    reply_markup = None
+
+                try:
+                    madflix_msg = await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML,
+                                                 reply_markup=reply_markup, protect_content=True)
+                    madflix_msgs.append(madflix_msg)
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                    madflix_msg = await msg.copy(chat_id=message.from_user.id, caption=caption,
+                                                 parse_mode=ParseMode.HTML, reply_markup=reply_markup,
+                                                 protect_content=True)
+                    madflix_msgs.append(madflix_msg)
+                except:
+                    pass
+
+            try:
+                await client.send_sticker(chat_id=message.from_user.id,
+                                          sticker="CAACAgUAAxkBAAI6eWjpNJUmsaD6O-PzuDOtGxZDg95lAAJFHAACwutJV4qF4DMw0uAwHgQ")
+            except:
+                pass
+
+            k = await client.send_message(chat_id=message.from_user.id,
+                                          text=f"<b>❗️ <u><i>Iᴍᴘᴏʀᴛᴀɴᴛ</i></u> ❗️</b>\n\n"
+                                               f"<b><i>💢 Fɪʟᴇs Wɪʟʟ ʙᴇ Dᴇʟᴇᴛᴇᴅ ɪɴ {file_auto_delete} (Dᴜᴇ ᴛᴏ Cᴏᴘʏʀɪɢʜᴛ Issᴜᴇs).\n\n"
+                                               f"💢 Sᴀᴠᴇ Tʜᴇsᴇ Fɪʟᴇs ᴛᴏ ʏᴏᴜʀ Sᴀᴠᴇᴅ Mᴇssᴀɢᴇs Aɴᴅ Dᴏᴡɴʟᴏᴀᴅ Tʜᴇʀᴇ 📂</i></b>")
+
+            asyncio.create_task(delete_files(madflix_msgs, client, k))
+            return
+
+        elif len(argument) == 3:
             try:
                 start = int(int(argument[1]) / abs(client.db_channel.id))
                 end = int(int(argument[2]) / abs(client.db_channel.id))
@@ -63,7 +133,7 @@ async def start_command(client: Client, message: Message):
             return
         await temp_msg.delete()
 
-        madflix_msgs = []  # List to keep track of sent messages
+        madflix_msgs = []
 
         for msg in messages:
             if bool(CUSTOM_CAPTION) & bool(msg.document):
@@ -90,7 +160,6 @@ async def start_command(client: Client, message: Message):
             except:
                 pass
 
-        # 🔹 Send sticker after sending all files
         try:
             await client.send_sticker(chat_id=message.from_user.id,
                                       sticker="CAACAgUAAxkBAAI6eWjpNJUmsaD6O-PzuDOtGxZDg95lAAJFHAACwutJV4qF4DMw0uAwHgQ")
@@ -102,7 +171,6 @@ async def start_command(client: Client, message: Message):
                                            f"<b><i>💢 Fɪʟᴇs Wɪʟʟ ʙᴇ Dᴇʟᴇᴛᴇᴅ ɪɴ {file_auto_delete} (Dᴜᴇ ᴛᴏ Cᴏᴘʏʀɪɢʜᴛ Issᴜᴇs).\n\n"
                                            f"💢 Sᴀᴠᴇ Tʜᴇsᴇ Fɪʟᴇs ᴛᴏ ʏᴏᴜʀ Sᴀᴠᴇᴅ Mᴇssᴀɢᴇs Aɴᴅ Dᴏᴡɴʟᴏᴀᴅ Tʜᴇʀᴇ 📂</i></b>")
 
-        # Schedule the file deletion
         asyncio.create_task(delete_files(madflix_msgs, client, k))
         return
     else:
@@ -128,7 +196,6 @@ async def start_command(client: Client, message: Message):
         )
         return
 
-# ------------------ /start command for users not joined ------------------
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
     buttons = [
@@ -161,7 +228,6 @@ async def not_joined(client: Client, message: Message):
         disable_web_page_preview=True
     )
 
-# ------------------ /users command ------------------
 @Bot.on_message(filters.command("users") & filters.private)
 async def get_users(client: Bot, message: Message):
     msg = await message.reply_text(
@@ -180,7 +246,6 @@ async def get_users(client: Bot, message: Message):
 """
     )
 
-# ------------------ /broadcast command ------------------
 @Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
 async def send_text(client: Bot, message: Message):
     if message.reply_to_message:
@@ -228,9 +293,8 @@ async def send_text(client: Bot, message: Message):
         await asyncio.sleep(8)
         await msg.delete()
 
-# ------------------ Function to handle file deletion ------------------
 async def delete_files(messages, client, k):
-    await asyncio.sleep(FILE_AUTO_DELETE)  # Wait for the duration specified in config.py
+    await asyncio.sleep(FILE_AUTO_DELETE)
     for msg in messages:
         try:
             await client.delete_messages(chat_id=msg.chat.id, message_ids=[msg.id])
