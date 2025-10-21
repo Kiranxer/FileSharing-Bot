9eedd2ecc4860e159e9ae1a33cfee5f7b642072f
NeonAn <justasneon@gmail.com>
2025-10-21 10:27:23 +0530

    Add files via upload

--- database/database.py
+++ database/database.py
@@ -1,4 +1,15 @@
-#Database.py
+# ---------------------------------------------------
+# File Name: Database.py
+# Author: NeonAnurag
+# GitHub: https://github.com/MyselfNeon/
+# Telegram: https://t.me/MyelfNeon
+# YouTube: https://youtube.com/@MyselfNeon
+# Created: 2025-10-21
+# Last Modified: 2025-10-22
+# Version: Latest
+# License: MIT License
+# ---------------------------------------------------
+
 import pymongo, os
 from config import DB_URL, DB_NAME
 
@@ -28,6 +39,7 @@ async def full_userbase():
 async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
     return
+
 
 # MyselfNeon
 # Don't Remove Credit 🥺
