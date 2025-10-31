8d878f3516973a315b6df3fa25604f6a228d7b9a
NeonAn <justasneon@gmail.com>
2025-10-31 20:25:37 +0530

    Update anime.py

--- plugins/anime.py
+++ plugins/anime.py
@@ -19,8 +19,8 @@ from neonfiles import script
 
 @Bot.on_message(filters.command('anime') & filters.private)
 async def send_anime_texts(client: Client, message: Message):
-    max_lines_per_page = 45
-    max_chars_per_page = 5000
+    max_lines_per_page = 40
+    max_chars_per_page = 3800
     pages = []
     current_page = []
 
